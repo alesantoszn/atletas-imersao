d35d8a2cf9011e257097fd3031cf9c5da532dda3
Alexandre Saraiva <98625808+alesantoszn@users.noreply.github.com>
2024-09-10 14:06:16 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,32 @@
+// Criando listas [sempre entre colchetes[]]
+// criando objetos dentro das listas {sempre entre chaves{}}
+ // Array de objetos que representam informações sobre personalidades das artes marciais
+let dados = [
+        {
+          titulo: "Grão-Mestre Adriano Silva",
+          descricao: "Adriano Silva é uma figura proeminente no mundo das artes marciais, especialmente no Contato Total Mundial. Reconhecido como Grão- Mestre, ele é reverenciado por sua vasta experiência, dedicação e contribuições significativas para o desenvolvimento da modalidade. Através de sua liderança e ensinamentos, Adriano Silva inspirou inúmeros praticantes e academias ao redor do mundo, consolidando o Contato Total como uma das artes marciais mais completas e eficazes da atualidade.",
+          link: "https://forcaeluta.blogspot.com/2015/11/artes-marciais-entrevista-exclusiva-com.html",
+          tags: "CTM Brother Figth BF"
+        },
+        {
+          titulo: "Maguila",
+          descricao: "José Adilson Rodrigues dos Santos, mais conhecido como Maguila, ex-pugilista brasileiro, marcou época no boxe nacional e internacional. Conhecido por sua técnica apurada e garra, conquistou diversos títulos e reconhecimento mundial. Sua trajetória inspirou gerações de boxeadores e deixou um legado duradouro para o esporte.",
+          link: "https://pt.wikipedia.org/wiki/Maguila",
+          tags: "welcome"
+        },
+        {
+          titulo: "Hélio Gracie",
+          descricao: "Hélio Gracie foi um mestre de jiu-jitsu brasileiro, fundador da Gracie Jiu-Jitsu. Considerado um dos pioneiros das artes marciais mistas, seus ensinamentos revolucionaram o mundo das lutas e influenciaram inúmeras modalidades. Sua filosofia e técnicas tornaram o jiu-jitsu uma das artes marciais mais populares do mundo.",
+          link: "https://pt.wikipedia.org/wiki/Fam%C3%ADlia_Gracie",
+          tags: "oss"
+        },
+        {
+          titulo: "The Drunken Master",
+          descricao: "Emanuel Augustus, conhecido como 'The Drunken Master', foi um ex-pugilista americano famoso por seu estilo de luta imprevisível e emocionante. Com uma carreira marcada por lutas eletrizantes e uma disposição para enfrentar qualquer adversário, Augustus conquistou um grande número de fãs ao redor do mundo. Seu legado inclui uma série de lutas memoráveis e um impacto significativo no boxe independente.",
+          link: "https://en.wikipedia.org/wiki/Emanuel_Augustus",
+          tags: "showman"
+      },
+      ];
+    
+
+
